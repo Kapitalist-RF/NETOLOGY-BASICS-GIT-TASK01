9499151be47a7c6b7ee79d037e67b827a323aa3a
Kapitalist-RF <den.ry97@gmail.com>
2023-01-11 23:56:03 +0300

    Add method MIN and AVERAGE

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         SalesManager salesManager = new SalesManager(new int[] {1, 2, 3, 4, 5});
         System.out.println(salesManager.max());
+        System.out.println(salesManager.min());
+        System.out.println(salesManager.average());
     }
 }
